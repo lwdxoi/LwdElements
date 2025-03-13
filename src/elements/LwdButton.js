221614246496c492ed864b94b3c4d2f7e0df3f2f
lwdxoi <lwdxoi@proton.me>
2025-03-12 23:33:22 -0300

    Make LwdButton the button itself without other tag in shadowDOM

--- src/elements/LwdButton.js
+++ src/elements/LwdButton.js
@@ -1,8 +1,7 @@
 class LwdButton extends LwdElement  {
   constructor(props = {}) {
-    const btn = document.createElement('button')
-    btn.append(new LwdSlot())
-    super(btn, props);
+    super(undefined, props);
+    this.shadowRoot.append(new LwdSlot())
     this.type ||= 'primary'
   }
 
@@ -10,60 +9,30 @@ class LwdButton extends LwdElement  {
     return [...super.observedAttributes, 'value', 'type', 'name'];
   }
 
-  get name(){
-    return this.getAttribute('name')
-  }
-  set name(value){
-    this.setAttribute('name', value)
-    this.root.setAttribute('name', value)
-  }
-
-  get type(){
-    return this.getAttribute('type')
-  }
-  set type(value){
-    this.setAttribute('type', value)
-    this.root.setAttribute('type', value)
-  }
-
-  get value(){
-    return this.getAttribute('value')
-  }
-  set value(value){
-    this.setAttribute('value', value)
-    this.root.value = value
-  }
-
-
   get styleSheet() {
     return `${super.styleSheet}
-
 :host{
-  --primary-color: black;
-  --primary-bg: white;
-  --primary-border: none;
-  --ghost-color: white;
-  --ghost-bg:rgba(0, 0, 0, 0);
-  --ghost-border: none;
-  --outline-color: white;
-  --outline-bg:none;
-  --outline-border: white solid 2px;
-}
-:host{
-  display: inline-grid;
-  color: var(--${this.type}-color);
-  backgroud: var(--${this.type}-bg);
-  border: var(--${this.type}-border);
-}
-
-:host > button {
-  height: 100%;
-  width: 100%; 
-  border-radius: 0.4rem;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: center;
   padding: 2px 0.5rem;
-  color: inherit;
-  background: inherit;
-  border: inherit;
+  border-radius: 0.4rem;
+  cursor: pointer;
+}
+:host([type=primary]){
+  background: var(--primary-bg, white);
+  color: var(--primary-color, black);
+  border: var(--primary-border, none);
+}
+:host([type=outline]){
+  background: var(--outline-bg, none);
+  color: var(--outline-color, white);
+  border: var(--outline-color, white) var(--outline-border, solid 2px);
+}
+:host([type=ghost]){
+  background: var(--ghost-bg, #88888820);
+  color: var(--ghost-color, white);
+  border: var(--ghost-border, none);
 }
     `
   }
