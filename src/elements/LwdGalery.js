c575589ddc785872b517507707a20c08fc5ac967
lwdxoi <lwdxoi@proton.me>
2025-04-30 23:56:01 -0300

    Finish implementing focus in LwdGalery

--- src/elements/LwdGalery.js
+++ src/elements/LwdGalery.js
@@ -1,16 +1,17 @@
-class LwdGalery extends LwdElement  {
+class LwdGalery extends LwdElement {
   constructor(props = {}) {
     // super(document.createElement('div'), props);
     // this.root.append(new LwdSlot({name: 'image'}))
     super(undefined, props);
-    this.shadowRoot.append(new LwdSlot({name: 'image'}))
-    this.shadowRoot.append(this.focusSection = new LwdSection({className: 'focus'}))
+    this.shadowRoot.append(new LwdSlot({ name: 'image' }))
+    this.shadowRoot.append(this.focusSection = new LwdSection({ className: 'focus' }))
     this.focusSection.append(this.focusSection.image = new LwdImage())
+    this.focusSection.append(this.focusSection.video = new LwdVideo())
 
-    this.focusSection.ondblclick = () => {this.focusSection.classList.remove('active'); this.focusSection.image.src=undefined;}
+    this.hideFocus()
+    this.focusSection.ondblclick = () => this.hideFocus()
 
-    this.addEventListener('attribute-changed', ({ detail: { name, newValue } }) => name == '_focus-image' ? this.renderFocus() : '')
-
+    this.addEventListener('attribute-changed', ({ detail: { name, newValue } }) => (name == '_focus-image' && newValue != undefined) ? this.renderFocus() : '')
   }
 
   static get observedAttributes() {
@@ -20,18 +21,35 @@ class LwdGalery extends LwdElement  {
     return [...super.reRenderOnChange, '_image-list'];
   }
 
-  renderFocus(){
-    this.focusSection.image.src = this.focusImage.srcOriginal
+  renderFocus() {
+    if (this.focusImage.srcOriginal.includes('.mp4')) {
+      this.focusSection.video.style.display = ''
+      this.focusSection.video.src = this.focusImage.srcOriginal
+    } else {
+      this.focusSection.image.style.display = ''
+      this.focusSection.image.src = this.focusImage.srcOriginal
+
+    }
     this.focusSection.classList.add('active')
+  }
+
+  hideFocus(){
+    this.focusSection.classList.remove('active'); 
+    this.focusImage = undefined;
+    
+    this.focusSection.video.src = undefined
+    this.focusSection.video.style.display = 'none'
+    this.focusSection.image.src = undefined
+    this.focusSection.image.style.display = 'none'
   }
 
 
   render() {
     super.render()
-    this.querySelectorAll('[slot=image]').forEach((el ) => el.remove())
+    this.querySelectorAll('[slot=image]').forEach((el) => el.remove())
     console.log('LwdGalery render', this.imageList)
     this.imageList?.forEach((image) => {
-      this.append(new LwdImage({...image, src: image.srcThumb, slot: 'image', onclick: () => this.focusImage = image}))
+      this.append(new LwdImage({ ...image, src: image.srcThumb, slot: 'image', onclick: () => this.focusImage = image }))
     });
   }
 
@@ -43,10 +61,8 @@ class LwdGalery extends LwdElement  {
   max-width: 100%;
   height: 100%;
   width: 100%;
-  --image-width: ${this.imageWidth || '134px'};
-  --image-height: ${this.imageHeight || '140px'};
-  grid-template-columns: repeat(auto-fill, var(--image-width));
-  grid-template-rows: repeat(auto-fill, var(--image-height));
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(6, calc( 100% / 7 ));
   grid-gap: 8px;
   align-items: center;
   justify-items: center;
